b109e44017a0f8adbf1f1fe4c78be9abed1a6ad2
NoneSL <601064722@qq.com>
2024-08-22 18:14:18 +0800

    feat:功能

---
+++ src/views/layout/components/menu-view.vue
@@ -0,0 +1,62 @@
+<script setup lang='ts'>
+import { ref } from 'vue';
+
+defineOptions({
+    name: 'menuView'
+})
+const activeKey = ref('3');
+const menu = ref([
+    {
+        menu: 'tab1',
+        key: "3",
+        children: [
+            { text: "1" },
+            { text: "1" },
+            { text: "1" },
+            { text: "1" }
+        ]
+    },
+    {
+        menu: 'tab2',
+        key: "4",
+
+        children: [
+            { text: "1" }
+        ]
+    },
+])
+</script>
+
+<template>
+    <div>
+        <a-tabs v-model:activeKey="activeKey">
+            <a-tab-pane :key="item.key" v-for="item in menu" :tab="item.menu">
+                <div class="container-icon">
+                    <div v-for="icon in item.children">{{ icon.text }}</div>
+                </div>
+            </a-tab-pane>
+        </a-tabs>
+    </div>
+</template>
+
+<style lang='scss' scoped>
+.container-icon {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+
+    div {
+        width: 100px;
+        height: 100px;
+        border: 1px solid #000;
+        margin: 10px;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+}
+</style>
--- src/views/three/ammo/terrain.vue
+++ src/views/three/ammo/terrain.vue
@@ -4,6 +4,10 @@ import Ammo from 'ammojs-typed'
 import * as Three from "three"
 import { nextTick, onMounted, ref } from 'vue';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js"
+// import { Static } from 'vue';
+// import Stats from 'three/addons/libs/stats.module.js';
+import Stats from "three/examples/jsm/libs/stats.module.js"
 defineOptions({
     name: 'terrain'
 })
@@ -22,20 +26,114 @@ let physicsWorld: Ammo.btDiscreteDynamicsWorld;
 let cube: Three.Mesh;
 let transformAux1: Ammo.btTransform;
 let ctrl: OrbitControls;
-Ammo().then(api => {
+let camera: Three.Camera;
+let light: Three.DirectionalLight;
+let scene: Three.Scene;
+let physicsList: Three.Mesh[] = [];
+let stats = new Stats();
+Ammo.bind(window)().then(api => {
     console.log("ammo init ready")
     AmmoApi = api;
     init(api);
     ThreeInit();
-
+    statsInit();
+    // setInterval(() => {
+    //     addMesh();
+    // }, 1);
+    // setInterval(() => {
+    //     addMesh();
+    // }, 1);
     //   const v2 = new Ammo.btVector3(1, 2, 3) // <-- runtime error here
 })
 
 // }, 2000);
 const canvas = ref();
+//控制方块移动。
+const keys = {
+    'w': false,
+    's': false,
+    'a': false,
+    'd': false,
+}
+const moveCtrl = () => {
+    //控制状态。render中移动，
+
+    window.addEventListener('keydown', keydownHandler)
+    window.addEventListener('keyup', keyupHandler)
+    //解除状态。
+    function keyupHandler(e: KeyboardEvent) {
+        const key = e.key as keyof typeof keys
+        if (keys[key]) {
+            keys[key] = false;
+        }
+    }
+    //一次只能往一个方向移动
+    function keydownHandler(e: KeyboardEvent) {
+        if (Object.values(keys).filter(item => item).length > 1) {
+            return;
+        }
+        const key = e.key as keyof typeof keys;
+        keys[key] = true;
+    }
+
+}
+moveCtrl();
+const moveCtrlHandle = () => {
+    if (keys.w) {
+        cube.position.y += 0.1;
+    }
+    if (keys.s) {
+        cube.position.y -= 0.1;
+    }
+    if (keys.a) {
+        cube.position.x -= 0.1;
+    }
+    if (keys.d) {
+        cube.position.x += 0.1;
+    }
+}
+const statsInit = () => {
+    const elem = canvas.value;
+
+    stats.dom.style.position = 'absolute';
+    stats.dom.style.top = '0px';
+    console.log(stats.dom)
+    elem.appendChild(stats.dom);
+}
+const addObstacles = () => {
+    const boxGeometry = new Three.BoxGeometry(10, 10, 2);
+    const boxMaterial = new Three.MeshStandardMaterial({ color: 0xff0000 });
+    const Mesh = new Three.Mesh(boxGeometry, boxMaterial);
+    scene.add(Mesh);
+    const transform = new AmmoApi.btTransform();
+    transform.setIdentity();
+    const pos = Mesh.position;
+    transform.setOrigin(new AmmoApi.btVector3(pos.x, pos.y, pos.z));
+    const motionState = new AmmoApi.btDefaultMotionState(transform);
+
+
+    const physisc = new AmmoApi.btRigidBody(
+        new AmmoApi.btRigidBodyConstructionInfo(
+            1,
+            motionState,
+            new AmmoApi.btBoxShape(new AmmoApi.btVector3(5, 5, 1)),
+            new AmmoApi.btVector3(0, 0, 0)
+        )
+    );
+    physicsWorld.addRigidBody(physisc);
+}
+const loadModel = () => {
+    const loader = new FBXLoader();
+    loader.load('/public/model/three-axis-robot.FBX', (model) => {
+        console.log(model)
+
+        scene.add(model.children[0])
+    })
+
+}
 const ThreeInit = () => {
-    const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const scene = new Three.Scene();
+    camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    scene = new Three.Scene();
     scene.background = new Three.Color(0xeeeeee);
     const renderer = new Three.WebGLRenderer({ antialias: true });
     const elem = canvas.value;
@@ -48,36 +146,85 @@ const ThreeInit = () => {
 
     camera.position.set(10, 10, 10);
 
-    addMesh(scene, camera);
-    light(scene);
-
+    addMesh();
+    addObstacles();
+    addLight(scene);
+    createPlan();
+    loadModel();
     const render = () => {
         // console.log("rendering")
         renderer.render(scene, camera);
-        physicsWorld.stepSimulation(10)
-        const objPhys = cube.userData.physicsBody;
-        const ms = objPhys.getMotionState();
-        if (ms) {
+        light.shadow.camera.updateProjectionMatrix();
+        physicsWorld.stepSimulation(clock.getDelta())
+        // const objPhys = cube.userData.physicsBody;
+        physicsList.forEach(item => {
+            const objPhys = item.userData.physicsBody;
+            // objPhys.setLinearVelocity(new AmmoApi.btVector3(0, 10, 1))
 
-            ms.getWorldTransform(transformAux1);
-            const p = transformAux1.getOrigin();
-            const q = transformAux1.getRotation();
-            cube.position.set(p.x(), p.y(), p.z());
-            cube.quaternion.set(q.x(), q.y(), q.z(), q.w());
-            ctrl.target.set(cube.position.x, cube.position.y, cube.position.z)
+            // const transform1 = new AmmoApi.btTransform(new AmmoApi.btQuaternion(0, 0, 0, 1), new AmmoApi.btVector3(1, 10, 1));
+            // const btMotionState = new AmmoApi.btDefaultMotionState();
+            // btMotionState.setWorldTransform(transform1)
+            // objPhys.setMotionState(btMotionState)
 
-        }
+            // objPhys.setInvInertiaDiagLocal(new AmmoApi.btVector3(0, 1, 0))
+            // const transform = new AmmoApi.btTransform();
+            // transform.setIdentity();
+            // const pos = cube.position;
+            // transform.setOrigin(new AmmoApi.btVector3(pos.x, pos.y, pos.z));
+            // const motionState = new AmmoApi.btDefaultMotionState(transform);
+            // objPhys.setMotionState(motionState)
+            const ms = objPhys.getMotionState();
+            if (ms) {
+
+                ms.getWorldTransform(transformAux1);
+                const p = transformAux1.getOrigin();
+                const q = transformAux1.getRotation();
+                item.position.set(p.x(), p.y(), p.z());
+                item.quaternion.set(q.x(), q.y(), q.z(), q.w());
+                ctrl.target.set(item.position.x, item.position.y, item.position.z)
+
+            }
+            //移动控制。
+            //地板上的。
+            if (keys.w) {
+                // objPhys.set
+                objPhys.applyCentralImpulse(new AmmoApi.btVector3(10, 0, 0))
+            }
+            if (keys.s) {
+                objPhys.applyCentralImpulse(new AmmoApi.btVector3(-10, 20, 0))
+
+            }
+            if (keys.a) {
+                objPhys.applyCentralImpulse(new AmmoApi.btVector3(0, 0, 10))
+
+            }
+            if (keys.d) {
+                objPhys.applyCentralImpulse(new AmmoApi.btVector3(0, 0, -10))
+
+            }
+
+        })
+        stats.update();
+
         ctrl.update();
+        // const pos = cube.position;
+        // const transform1 = new AmmoApi.btTransform(new AmmoApi.btQuaternion(0, 0, 0, 0), new AmmoApi.btVector3(pos.x, pos.y, pos.z));
+        // const btMotionState = new AmmoApi.btDefaultMotionState();
+
+        // transformAux12.setRotation(new AmmoApi.btQuaternion(new AmmoApi.btVector3(0, 0, 0), 0));
+        // btMotionState.setWorldTransform(transform1)
+        // cube.userData.physicsBody.setMotionState(btMotionState);
+        // console.log("text")
 
         renderer.setAnimationLoop(render);
 
     }
     render();
 }
-const addMesh = (scene: Three.Scene, camera: Three.Camera) => {
+const addMesh = () => {
     cube = new Three.Mesh(new Three.BoxGeometry(1, 1, 1), new Three.MeshLambertMaterial({ color: 0x00ff00 }));
     cube.position.set(0, 100, 0);
-    camera.lookAt(cube.position);
+    // camera.lookAt(cube.position);
     scene.add(cube);
 
     const shape = new AmmoApi.btBoxShape(new AmmoApi.btVector3(1, 1, 1));
@@ -93,29 +240,27 @@ const addMesh = (scene: Three.Scene, camera: Three.Camera) => {
     const motionState = new AmmoApi.btDefaultMotionState(transform);
     const rbInfo = new AmmoApi.btRigidBodyConstructionInfo(mass, motionState, shape, localInertia);
     const body = new AmmoApi.btRigidBody(rbInfo);
+
     physicsWorld.addRigidBody(body);
     cube.userData.physicsBody = body;
 
-    const groundMaterial = new Three.MeshPhongMaterial({ color: 0xC7C7C7 });
-    const terrain = new Three.Mesh(new Three.PlaneGeometry(1000, 1000), groundMaterial);
-    const loader = new Three.TextureLoader();
 
-    loader.load("/public/texture/grid.png", texture => {
-        texture.wrapS = Three.RepeatWrapping;
-        texture.wrapT = Three.RepeatWrapping;
-        texture.repeat.set(100, 100);
-        groundMaterial.map = texture;
-        groundMaterial.needsUpdate = true;
-    })
-    terrain.position.set(0, 0, 0);
-    terrain.castShadow = true;
-    terrain.receiveShadow = true;
-    terrain.rotateX(-Math.PI / 2)
-    scene.add(terrain);
+    physicsList.push(cube)
+
 }
-const light = (scene: Three.Scene) => {
-    const light = new Three.DirectionalLight(0xffffff, 1);
+const addLight = (scene: Three.Scene) => {
+    light = new Three.DirectionalLight(0xffffff, 1);
     light.position.set(10, 10, 10);
+    light.shadow.mapSize.width = 1024; // Default: 512
+    light.shadow.mapSize.height = 1024; // Default: 512
+    light.shadow.camera.near = 0.5; // Default: 0.5
+    light.shadow.camera.far = 500; // Default: 500
+    light.shadow.bias = -0.005;
+
+    light.shadow.camera.left = -50;
+    light.shadow.camera.right = 50;
+    light.shadow.camera.top = 50;
+    light.shadow.camera.bottom = -50;
     // const dLight = 200;
     // const sLight = dLight * 0.25;
     // light.shadow.camera.left = - sLight;
@@ -132,7 +277,38 @@ const light = (scene: Three.Scene) => {
 
     scene.add(light);
 }
+const createPlan = () => {
+    const heightFieldShape = new AmmoApi.btStaticPlaneShape(new AmmoApi.btVector3(0, 1, 0), 0);
+    const groundShape = heightFieldShape;
+    const groundTransform = new AmmoApi.btTransform();
+    groundTransform.setIdentity();
+    // Shifts the terrain, since bullet re-centers it on its bounding box.
+    groundTransform.setOrigin(new AmmoApi.btVector3(0, 0, 0));
+    const groundMass = 0;
+    const groundLocalInertia = new AmmoApi.btVector3(0, 0, 0);
+    const groundMotionState = new AmmoApi.btDefaultMotionState(groundTransform);
+    const groundBody = new AmmoApi.btRigidBody(new AmmoApi.btRigidBodyConstructionInfo(groundMass, groundMotionState, groundShape, groundLocalInertia));
+    groundBody.setFriction(1);
+    physicsWorld.addRigidBody(groundBody);
+    transformAux1 = new AmmoApi.btTransform();
 
+    const groundMaterial = new Three.MeshBasicMaterial({ color: 0xC7C7C7 });
+    const terrain = new Three.Mesh(new Three.PlaneGeometry(1000, 1000), groundMaterial);
+    const loader = new Three.TextureLoader();
+
+    loader.load("/public/texture/grid.png", texture => {
+        texture.wrapS = Three.RepeatWrapping;
+        texture.wrapT = Three.RepeatWrapping;
+        texture.repeat.set(100, 100);
+        groundMaterial.map = texture;
+        groundMaterial.needsUpdate = true;
+    })
+    terrain.position.set(0, 0, 0);
+    // terrain.castShadow = true;
+    terrain.receiveShadow = true;
+    terrain.rotateX(-Math.PI / 2)
+    scene.add(terrain);
+}
 const init = (api: typeof Ammo) => {
     const collisionConfiguration = new api.btDefaultCollisionConfiguration();
     const dispatcher = new api.btCollisionDispatcher(collisionConfiguration);
@@ -141,20 +317,6 @@ const init = (api: typeof Ammo) => {
     physicsWorld = new api.btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
     // console.log(physicsWorld)
     physicsWorld.setGravity(new api.btVector3(0, - 60, 0));
-
-    const heightFieldShape = new api.btStaticPlaneShape(new api.btVector3(0, 1, 0), 0);
-    const groundShape = heightFieldShape;
-    const groundTransform = new api.btTransform();
-    groundTransform.setIdentity();
-    // Shifts the terrain, since bullet re-centers it on its bounding box.
-    groundTransform.setOrigin(new api.btVector3(0, 0, 0));
-    const groundMass = 0;
-    const groundLocalInertia = new api.btVector3(0, 0, 0);
-    const groundMotionState = new api.btDefaultMotionState(groundTransform);
-    const groundBody = new api.btRigidBody(new api.btRigidBodyConstructionInfo(groundMass, groundMotionState, groundShape, groundLocalInertia));
-    physicsWorld.addRigidBody(groundBody);
-    transformAux1 = new api.btTransform();
-
 }
 </script>
 
